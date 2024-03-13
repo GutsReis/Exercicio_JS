45cd47ff3d44a21adfb1ec0236d204b245203e38
Gustavo Reis <reisgg9@gmail.com>
2024-03-12 22:22:40 -0300

    Exercicio simples JS

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+function calculaArea(base, altura) {
+    return base * altura;
+}
+// Exemplo de uso da função:
+let areaRetangulo = calculaArea(5, 10); // Chamando a função e armazenando o resultado em uma variável
+console.log("A área do retângulo é:", areaRetangulo); // Imprimindo o resultado no console
